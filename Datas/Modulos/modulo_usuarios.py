ba67b979e14044c910eb92372a062938f5e7de30
andresDZ2024 <daza45daza@gmail.com>
2024-02-16 09:39:57 -0500

    nooooooo

---
+++ Datas/Modulos/modulo_usuarios.py
@@ -0,0 +1,99 @@
+import json
+
+def ingresar_numero(mensaje):
+    while True:
+        valor = input(mensaje)
+        if valor.isdigit():
+            return valor
+        else:
+            print("Ingresa un valor valido ")
+
+
+def registrar_usuario():
+    with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+
+    clientes = data["Clientes"] 
+
+    nuevo_cliente = {}
+    while True:    
+        try:
+            nuevo_id = max([cliente["ID"]for cliente in clientes], default = 0) + 1
+            nuevo_cliente["ID"] = nuevo_id
+            nuevo_cliente["N_documento"] = ingresar_numero("Ingrese el numero de documento del nuevo cliente: ")
+            nuevo_cliente["Nombre"] = input("Ingrese el nombre del nuevo cliente: ")
+            nuevo_cliente["Apellido"] = input("Ingrese el apellido del nuevo cliente: ")
+            nuevo_cliente["N_celular"] = ingresar_numero("Ingrese el numero de celular: ")
+            nuevo_cliente["Direccion"] = input("Ingrese la direccion del nuevo cliente: ")
+            nuevo_cliente["Servicios"] = []
+            clientes.append(nuevo_cliente)
+            break
+
+        except ValueError:
+            print("Ingresa un valor numerico")    
+    
+    
+
+    with open("Datas/Clientes.json", "w") as outfile:
+        json.dump(data, outfile, indent = 4)
+
+def mostrar_info_cliente():
+    with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+    clientes = data["Clientes"]    
+    while True:
+        try:
+            id_cliente = int(input("Ingresa el ID del cliente que deseas buscar: "))
+            cliente_encontrado = next((cliente for cliente in clientes if cliente["ID"] == id_cliente), None)
+            if cliente_encontrado:
+                for key, value in cliente_encontrado.items():
+                    print(f"{key}: {value}")
+                break       
+        except ValueError:
+            print("Ingresa un valor numerico")
+
+def actualizar_cliente():
+    with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+    clientes = data["Clientes"]
+    id_cliente = int(input("Ingresa el ID del cliente que deseas actualizar: "))
+    for cliente in clientes:
+        if cliente["ID"] == id_cliente:
+            N_documento = ingresar_numero("Ingresa el nuevo numero de documento: ")
+            Nombre = input("Ingresa el nombre: ")
+            Apellido = input("Ingresa el apellido: ")
+            N_celular = ingresar_numero("Ingresa el nuevo numero de celular: ")
+            Direccion = input("Ingrese la nueva direccion del cliente: ")
+
+
+        cliente["N_documento"] = N_documento    
+        cliente["Nombre"] = Nombre    
+        cliente["Apellido"] = Apellido   
+        cliente["N_celular"] = N_celular  
+        cliente["Direccion"] = Direccion
+
+    with open("Datas/Clientes.json", "w") as outfile:
+        json.dump(data, outfile, indent = 4)      
+
+def eliminar_cliente():
+    with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+    clientes = data["Clientes"]
+    id_cliente = int(input("Ingresa el ID del cliente que deseas eliminar: "))
+    for cliente in clientes:
+        if cliente["ID"] == id_cliente:
+            clientes.remove(clientes)
+
+def Registrar_servicios():
+    with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+    clientes = data["Clientes"]
+    servicio_registrado = set()
+    for cliente in clientes.get("Servicios", []):
+            s = input(f"Ingresa el servicio N. {cliente}")
+            servicio_registrado.add(s)
+
+
+
+
+                        
---
+++ menu.py
@@ -0,0 +1,48 @@
+import json
+import Datas.Modulos.modulo_usuarios as FCRUD
+
+def main():
+    print("Bienvenido usuario, ¿Que deseas hacer el día de hoy? ")
+    print("1. acceder al modulo de usuarios ")
+    print("2. acceder al modulo de gestion de servivios ")
+    print("3. acceder al modulo de reportes ")
+    print("4. acceder al modulo de bonificaciones ")
+    print("5. salir ")
+
+def menu_usuarios():
+    print("Menú de usuarios ")
+    print("1. registrar cliente nuevo ")
+    print("2. ver informacion de un cliente ")
+    print("3. actualizar informacion de un cliente ")
+    print("4. eliminar cliente ya existente ")
+    print("5. Registro de servicios utilizados por un cliente  ")
+    print("6. seguimiento de las interacciones de los clientes ")
+    print("7. mostrar los clientes más leales ")
+    print("8. atrás ")
+
+with open("Datas/Clientes.json", "r") as outfile:
+        data = json.load(outfile)
+while True:
+    main()
+    opcion = input("selecciona una opcion: ")
+    if opcion == '1':    
+        while True:
+            menu_usuarios()
+            opcion = input("selecciona una opcion: ")
+            if opcion == '1':
+                print("zi")
+                FCRUD.registrar_usuario()
+            elif opcion == '2':
+                FCRUD.mostrar_info_cliente()   
+            elif opcion == '3':
+                FCRUD.actualizar_cliente() 
+            elif opcion == '4':
+                FCRUD.eliminar_cliente()  
+            elif opcion == '5':
+                FCRUD.registrar_servicios()    
+            elif opcion == '8':
+                break    
+    elif opcion == '5':
+        print("Hasta luego")
+        break
+               
